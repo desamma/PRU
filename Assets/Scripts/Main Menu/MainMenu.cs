44c10d677fc6063390edccf5602ed106930171ad
JT2k4 <142780410+JT2k4@users.noreply.github.com>
2025-07-16 02:28:38 +0700

    Final map
    Final new map
    Die Win scene
    music boom boom

---
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Chọn Play Game.");
+        SceneManager.LoadScene(1); // hoặc SceneManager.LoadSceneAsync(1);
+    }
+
+    public void ContinueGame()
+    {
+        Debug.Log("Chọn Continue Game.");
+        if (SaveSystem.SaveExists())
+        {
+            PlayerData data = SaveSystem.LoadPlayer();
+            SceneManager.LoadSceneAsync(1).completed += (op) =>
+            {
+                TempLoader.CreateLoader(data);
+            };
+        }
+        else
+        {
+            Debug.LogWarning("Không có file save.");
+        }
+    }
+}
